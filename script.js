30d18c219fed1842eb1aea5d1cea2a091a75e986
omolaso <adextee001@gmail.com>
2022-09-12 04:09:54 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.getElementById('display');
+const buttons = Array.from(document.getElementsByClassName('button'));
+// console.log(button);
+
+buttons.map((button) => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = ''
+                break;
+            case '←':
+                display.innerText = display.innerText.slice(0, -1);
+                break;
+            case '√':
+                display.innerText = Math.sqrt(display.innerText);
+                break;
+            case '=':
+                try {
+                    display.innerText = eval(display.innerText)
+                } catch (error) {
+                    display.innerText = 'error'
+                }
+                break;
+            default: 
+                display.innerText += e.target.innerText
+        }
+    })
+})
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens:{
+      md: '786px'
+    },
+    extend: {
+      fontFamily:{
+        sans:['Poppins', 'sans-serif']
+      },
+      colors:{
+        black: '#000',
+        grey: '#ccc',
+        brown: '#A52A2A',
+        darkRed: '#8B0000',
+        maroon: '#800000'
+      }
+    },
+  },
+  plugins: [],
+}
